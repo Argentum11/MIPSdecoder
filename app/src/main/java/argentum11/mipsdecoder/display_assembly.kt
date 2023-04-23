10e09cdc71ceef31a47ef9c9a8e5a9ce2ac101a3
Leo Predator <leochang658@gmail.com>
2023-04-24 02:21:42 +0800

    FEAT: I type command

--- app/src/main/java/argentum11/mipsdecoder/display_assembly.kt
+++ app/src/main/java/argentum11/mipsdecoder/display_assembly.kt
@@ -21,10 +21,16 @@ class display_assembly : AppCompatActivity() {
     }
 
     private fun decodeOPCode(opcode:String): String{
-        if(opcode == "000000"){
-            return getString(R.string.r_type)
+        return when (opcode) {
+            "000000" -> getString(R.string.r_type)
+            "100011" -> getString(R.string.load_word)
+            "101011" -> getString(R.string.store_word)
+            "000100" -> getString(R.string.branch_if_equal)
+            "000101" -> getString(R.string.branch_if_not_equal)
+            "001100" -> getString(R.string.and_immediate)
+            "001101" -> getString(R.string.or_immediate)
+            else -> getString(R.string.unknown_format)
         }
-        else return getString(R.string.unknown_format)
     }
 
     private fun decodeRegister(registerBinary:String):String{
@@ -62,34 +68,45 @@ class display_assembly : AppCompatActivity() {
             "000010" -> getString(R.string.shift_right_logical)
             "101010" -> getString(R.string.set_if_less_than)
             "001000" -> getString(R.string.jump_register)
-            //else -> functionCode
             else -> "invalid function code"
         }
     }
 
     private fun decodeMachineCode():String{
         opcode = decodeOPCode(machineCode.substring(0, 6))
-        if (opcode == getString(R.string.r_type)){
-            val rs = decodeRegister(machineCode.substring(6, 11))
-            val rt = decodeRegister(machineCode.substring(11, 16))
-            val rd = decodeRegister(machineCode.substring(16, 21))
-            val shiftAmount = machineCode.substring(21, 26).toInt(radix = 2)
-            return when (val functionCode = decodeFunctionCode(machineCode.substring(26, 32))) {
-                getString(R.string.jump_register) -> {
-                    "jr \$$rs"
-                }
-                getString(R.string.shift_left_logical), getString(R.string.shift_right_logical) -> {
-                    "$functionCode \$$rd, \$$rt, $shiftAmount"
-                }
-                else -> {
-                    "$functionCode \$$rd, \$$rs, \$$rt"
+        val rs = decodeRegister(machineCode.substring(6, 11))
+        val rt = decodeRegister(machineCode.substring(11, 16))
+        val addressOrImmediate = machineCode.substring(16, 32).toInt(2)
+        when (opcode) {
+            getString(R.string.r_type) -> {
+                val rd = decodeRegister(machineCode.substring(16, 21))
+                val shiftAmount = machineCode.substring(21, 26).toInt(radix = 2)
+                return when (val functionCode = decodeFunctionCode(machineCode.substring(26, 32))) {
+                    getString(R.string.jump_register) -> {
+                        "jr \$$rs"
+                    }
+                    getString(R.string.shift_left_logical), getString(R.string.shift_right_logical) -> {
+                        "$functionCode \$$rd, \$$rt, $shiftAmount"
+                    }
+                    else -> {
+                        "$functionCode \$$rd, \$$rs, \$$rt"
+                    }
                 }
             }
+            getString(R.string.load_word), getString(R.string.store_word) -> {
+                return "$opcode \$$rt, $addressOrImmediate(\$$rs)"
+            }
+            getString(R.string.branch_if_equal), getString(R.string.branch_if_not_equal) -> {
+                return "$opcode \$$rs, \$$rt, ${addressOrImmediate*4}"
+            }
+            getString(R.string.and_immediate), getString(R.string.or_immediate) -> {
+                return "$opcode \$$rt, \$$rs, $addressOrImmediate"
+            }
+            else -> {
+                //return "invalid machine code"
+                return opcode
+            }
         }
-        else{
-            return "invalid machine code"
-        }
-
     }
 
 }
